51eb79efa30482935bd9cabf14dc55ee2ba896b8
Aeriel <aerielellisk5@gmail.com>
2022-12-29 19:57:54 +0000

    learned about forloops and output in terraform

--- main.tf
+++ main.tf
@@ -13,11 +13,32 @@ resource "docker_image" "nodered_image" {
   name = "nodered/node-red:latest"
 }
 
+resource "random_string" "random" {
+  count = 1
+  length = 4
+  special = false
+  upper = false
+}
+
 resource "docker_container" "nodered_container" {
-  name  = "nodered"
+  count = 1
+  name  = join("-",["nodered", random_string.random[count.index].result])
   image = docker_image.nodered_image.latest
   ports {
     internal = 1880
-    external = 1880
+    # external = 1880
   }
 }
+
+output "IP-ADDRESS" {
+  value = [for i in docker_container.nodered_container[*]: join(":", [i.ip_address],i.ports[*]["external"])]
+  description = "The IP address of the container"
+}
+
+output "container-name" {
+  value = docker_container.nodered_container[*].name
+  description = "The name of the container"
+}
+
+
+
